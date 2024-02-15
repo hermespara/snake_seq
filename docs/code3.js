ec3bb082257c93edec2d520f8b4529de4c746849
hermespara <paraqindes.hermes@gmail.com>
2024-02-15 19:00:42 +0100

    update

---
+++ docs/code3.js
@@ -0,0 +1,18 @@
+gdjs.snake_95final_95sceneCode = {};
+
+
+gdjs.snake_95final_95sceneCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.snake_95final_95sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.snake_95final_95sceneCode.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['snake_95final_95sceneCode'] = gdjs.snake_95final_95sceneCode;
